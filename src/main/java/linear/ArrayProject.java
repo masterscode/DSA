161d1049cc78b54107cd14565d940d569ff29b07
masterscode <ogbinakaemmanuel@gmail.com>
2021-09-08 14:48:56 +0100

    ..

---
+++ src/main/java/linear/ArrayProject.java
@@ -0,0 +1,11 @@
+package linear;
+
+public class ArrayProject {
+
+    private int days;
+
+    public ArrayProject(int numOfDays){
+        System.out.println("how many days temperature");
+        days = numOfDays;
+    }
+}
