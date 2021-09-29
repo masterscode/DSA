1d0da478ca6078903550746c108869b23da40403
masterscode <ogbinakaemmanuel@gmail.com>
2021-09-29 21:17:29 +0100

    add last and first

--- src/main/java/linear/linked_lists/LinkedLs.java
+++ src/main/java/linear/linked_lists/LinkedLs.java
@@ -8,19 +8,24 @@ public class LinkedLs {
 
     public void addLast(int value){
         var node = new Node(value);
-
-
         // 1. check to see if the linked-list is empty
-
-        if (first == null)  first = last = node;
+        if (isEmpty())  first = last = node;
         else{
             last.next = node;
             last = node;
         }
-
-
     }
 
+    public void addFirst(int value){
+        Node  node  = new Node(value);
+        if (isEmpty()) first = last = node;
+        else {
+            node.next = first;
+            first = node;
+        }
+    }
+
+    private boolean isEmpty(){return first == null;}
     private class Node{
         private int value;
         private Node next;
