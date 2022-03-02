3f28540abeaa4ec252afdcf0a5af74d0ed2cfb66
Decagon <decagon@Decagons-MacBook-Pro.local>
2022-03-02 11:49:35 +0100

    added half step contains

--- src/main/java/data_structures/search/LinearSearch.java
+++ src/main/java/data_structures/search/LinearSearch.java
@@ -15,8 +15,28 @@ public class LinearSearch {
         }
         return result;
     }
+     public static int halfSearch(int[] array, int v) {
+        int result = -1;
+        int halfLength = array.length / 2;
+        for (int index = 0; index < halfLength; index++) {
+            if (array[index] == v) {
+                result = index;
+                break;
+            }
+            int rearIndex =array.length - 1-  index;
+            if (array[rearIndex] == v){
+                result = rearIndex;
+                break;
+            }
+        }
+        return result;
+    }
+
+
+
 
     public static void main(String[] args) {
-        log.info(String.valueOf(LinearSearch.lSearch(new int[]{1, 2, 3, 4, 3, 3, 3, 5, 6, 3, 7, 8, 9}, 15)));
+//        log.info(String.valueOf(LinearSearch.lSearch(new int[]{1, 2, 3, 4, 3, 3, 3, 5, 6, 3, 7, 8, 9}, 15)));
+        log.info(String.valueOf(LinearSearch.halfSearch(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 3}, 3)));
     }
 }
