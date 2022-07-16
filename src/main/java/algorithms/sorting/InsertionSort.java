1b3ae6e5caaaf2af958281e090dcca2caae09156
emmanuel ogbinaka <ogbinakaemmanuel@gmail.com>
2022-07-16 21:04:21 +0100

    ..math search algos

--- src/main/java/algorithms/sorting/InsertionSort.java
+++ src/main/java/algorithms/sorting/InsertionSort.java
@@ -8,18 +8,19 @@ import lombok.extern.slf4j.Slf4j;
 public class InsertionSort {
 
     public static void main(String[] args) {
-        int[] array = new int[]{2, 5, 6, 4, 6, 7, 1};
-        sort(array);
-        log.info(Arrays.toString(array));
+        int[] array = new int[] { 2, 5, 6, 4, 6, 7, 1 };
+        // sort(array);
+        // log.info(Arrays.toString(array));
+        moveValues(array);
     }
 
     public static void sort(int[] array) {
         for (int i = 1; i < array.length; i++) {
-            var current = array[i];
+            int current = array[i];
 
-            var j = i - 1;
+            int j = i - 1;
 
-            while (j >= 0 && array[j] > current){
+            while (j >= 0 && array[j] > current) {
                 array[j + 1] = array[j];
                 j--;
             }
@@ -27,16 +28,26 @@ public class InsertionSort {
         }
     }
 
+    public static void sortX(int[] array) {
 
-    public static void sortX(int[] array){
-        
         for (int i = 1; i < array.length; i++) {
             int current = array[i];
             int j = i - 1;
-            while(j >= 0 && j > current){
+            while (j >= 0 && j > current) {
                 array[j + 1] = array[j];
-                j--; 
+                j--;
             }
         }
+    }
+
+    public static void moveValues(int[] array) {
+        log.info(Arrays.toString(array));
+        int i = array.length - 2;
+        while (i >= 0) {
+            array[i + 1] = array[i];
+            i--;
+        }
+        
+        log.info(Arrays.toString(array));
     }
 }
